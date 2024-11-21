0ca7bb1c34979dde3d2e4e7ff2f29d20cac4464d
hwan <prh0108@naver.com>
2024-11-22 02:43:08 +0900

    test

---
+++ src/main/java/com/example/cheongpodo/CheongpodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheongpodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongpodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheongpodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cheongpodo/CheongpodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheongpodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheongpodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
