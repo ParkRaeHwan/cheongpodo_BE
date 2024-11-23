4ca69af69eecb75ce14666c283e0ee681dc5ed58
RaeHwan Park <prh0108@naver.com>
2024-11-24 03:50:51 +0900

    last update

--- src/main/java/com/example/cheongpodo/controller/LocationController.java
+++ src/main/java/com/example/cheongpodo/controller/LocationController.java
@@ -3,7 +3,8 @@ package com.example.cheongpodo.controller;
 import com.example.cheongpodo.domain.KakaoCoordsResponse;
 import com.example.cheongpodo.domain.KakaoFoodPlaceResponse;
 import com.example.cheongpodo.service.KakaoApiService;
-import com.example.cheongpodo.service.OpenApiService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,21 +16,21 @@ import java.util.List;
 public class LocationController {
 
     private final KakaoApiService kakaoApiService;
-    private final OpenApiService openApiService;
 
-
+    @Operation(summary = "입력한 주소의 위도, 경도값 조회 API",
+                description = "클라이언트가 요청한 주소값에 대한 위도, 경도 API")
+    @Parameter(name = "address", description = "클라이언트가 요청한 주소 입력")
     @GetMapping("/search-position")
     public KakaoCoordsResponse.Document searchPosition(@RequestParam("address") String address) {
         return kakaoApiService.getKakaoPosition(address);
     }
 
+    @Operation(summary = "입력한 주소 근처 음식점 조회 API",
+            description = "클라이언트가 요청한 주소값 반경 2km내 음식점을 조회해주는 API")
+    @Parameter(name = "address", description = "클라이언트가 요청한 주소 입력")
     @GetMapping("/search-food")
     public List<KakaoFoodPlaceResponse.Document> searchFoodPlace(@RequestParam("address") String address) {
         return kakaoApiService.getKakaoFoodPlace(address);
     }
 
-//    @GetMapping("/search-place")
-//    public void searchPlace(@RequestParam("address") String address) {
-//        openApiService.getOpenPlace(address);
-//    }
 }
--- src/main/java/com/example/cheongpodo/domain/KakaoFoodPlaceResponse.java
+++ src/main/java/com/example/cheongpodo/domain/KakaoFoodPlaceResponse.java
@@ -8,6 +8,19 @@ import java.util.List;
 @Data
 public class KakaoFoodPlaceResponse {
     List<Document> documents;
+    private Meta meta;
+
+    @Data
+    public static class Meta {
+        @JsonProperty("total_count")
+        public int totalCount;
+
+        @JsonProperty("pageable_count")
+        public int pageableCount;
+
+        @JsonProperty("is_end")
+        public boolean isEnd;
+    }
 
     @Data
     public static class Document {
@@ -19,6 +32,9 @@ public class KakaoFoodPlaceResponse {
 
         @JsonProperty("place_url")
         private String place_url;
+
+        @JsonProperty("distance")
+        private String distance;
 
         @JsonProperty("x")
         private String longitude; // 경도
